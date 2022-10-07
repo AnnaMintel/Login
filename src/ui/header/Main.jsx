76a128779adc7590133eaa56bc93c20f0257a571
AnnaMintel <annikamintel99@yandex.ru>
2022-10-07 22:22:17 +0300

    feat: added styles to Main component

--- src/ui/header/Main.jsx
+++ src/ui/header/Main.jsx
@@ -1,33 +1,28 @@
 import React from "react";
-import { connect, useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Navigate, NavLink } from "react-router-dom";
 import { logOut, me } from "../../redux-bll/authReducer";
-import '../../App.css';
+import "../../App.css";
+import s from "../../ui/header/Main.module.css";
 
 export const Main = () => {
   let isAuth = useSelector((state) => state.auth.isAuth);
-  let userInfo = useSelector((state) => state.auth.userInfo);
   const dispatch = useDispatch();
 
-  const onLogOutClick = () =>  dispatch(logOut());
+  const onLogOutClick = () => dispatch(logOut());
 
   if (!isAuth) {
     return <Navigate to={"/"} />;
   }
 
   return (
-    <div>
-      {isAuth && (
-        <div>
-          {userInfo.userName} - <span onClick={onLogOutClick}><NavLink to="login">Log Out</NavLink></span>
-        </div>
-      )}
-      {!isAuth && (
-        <div>
-          <NavLink to="login">Sign In</NavLink>
-        </div>
-      )}
+    <div className={s.mainBlock}>
+      <h1>Welcome!</h1>
+      <button onClick={onLogOutClick}>
+        <NavLink className={s.navLink} to="/">
+          Log Out
+        </NavLink>
+      </button>
     </div>
   );
 };
-
